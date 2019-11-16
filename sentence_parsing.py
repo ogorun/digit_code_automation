669c5aa4346118f17fad38d127d924212d157cc7
olga-gorun <olga@anyclip.com>
2019-11-16 21:54:30 +0200

    sentence parsing with sentence tree

--- sentence_parsing.py
+++ sentence_parsing.py
@@ -1,37 +1,50 @@
-import nltk
+from nltk.parse.corenlp import CoreNLPParser
 from word2number import w2n
 
+parser = CoreNLPParser()
+
+
+def find_positive_sentences(node):
+    candidates = list(node.subtrees(lambda t: t.label() == 'S'))
+    sentences = []
+    for candidate in candidates:
+        exclude = False
+        subtrees = list(candidate.subtrees())
+        for subtree in subtrees:
+            if subtree != candidate:
+                if subtree.label() == 'S':
+                    exclude = True
+                break
+        if not exclude:
+            sentences.append(candidate)
+    return sentences
+
+
 def extract_condition_from_sentence(sentence):
-    grammar = r"""
-NUMBER_CLAUSE: {<CD><NN|NNS>}
-VALIDATION: {<RB|JJ><VBD|VBN|NN|NNS>}
-SingleCondition: {<NUMBER_CLAUSE>(<VBZ|VBP>)<JJ><CC><VALIDATION>}
-NothingCondition: {<NN><VBZ><JJ>}
-"""
-    parser = nltk.RegexpParser(grammar)
+    for parse_tree_candidate in parser.raw_parse(sentence):
+        #print(parse_tree_candidate)
+        data = {'valid_and_placed': 0, 'valid_on_invalid_places': 0}
 
-    sent_pos = nltk.pos_tag(sentence.split())
-    #print(sent_pos)
-    res = parser.parse(sent_pos)
-    #print(res)
-    data = {'valid_and_placed': 0, 'valid_on_invalid_places': 0}
-    for subtree in res.subtrees():
-        if subtree.label() == 'NothingCondition':
-            #print('Nothing', {'valid_and_placed': 0, 'valid_on_invalid_places': 0})
-            pass
-        elif subtree.label() == 'SingleCondition':
-            #print('Single', subtree)
-            assert subtree[0].label() == 'NUMBER_CLAUSE'
-            assert subtree[0][0][1] == 'CD'
-            number = w2n.word_to_num(subtree[0][0][0])
-            assert subtree[-1].label() == 'VALIDATION'
-            if subtree[-1][0][0] == 'well':
-                key = 'valid_and_placed'
-            else:
-                key = 'valid_on_invalid_places'
-            data[key] = number
+        for node in parse_tree_candidate:
+            found = False
+            subsentences = find_positive_sentences(node)
+            for subsentence  in subsentences:
+                if subsentence is not None:
+                    try:
+                        np_clause = next(subsentence.subtrees(lambda t: t.label() == 'NP'))
+                        number = w2n.word_to_num(next(np_clause.subtrees(lambda t: t.label() == 'CD'))[0])
+                        vp_clause = next(subsentence.subtrees(lambda t: t.label() == 'VP'))
+                        correct_clause = next(vp_clause.subtrees(lambda t: t.label() == 'ADJP' and 'correct' in t.leaves()))
+                        is_valid = 'well' in correct_clause.leaves()
+                        #print(number, is_valid)
+                        data['valid_and_placed' if is_valid else 'valid_on_invalid_places'] = number
+                        found = True
+                    except StopIteration:
+                        pass
+            if found:
+                return data
 
-    return data
+        return {'valid_and_placed': 0, 'valid_on_invalid_places': 0}
 
 
 if __name__ == '__main__':
